66a9209c471f6eb8acee8398e6190213931b04ab
md-rhyhan <md.rhyhan007@gmail.com>
2023-06-17 12:36:14 +0600

    Initial Commit

---
+++ Models/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace TestProject.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Email { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+
+
+    public static List<Student> GetAllStudent()
+    {
+        int id = 1;
+
+        return new List<Student>()
+        {
+            new() { Id = id++, Name = "Hasan", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "wrewer", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "werwer", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "dgdfg", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "rerr", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "gghfgh", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "fcvbcb", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "Hasan", Email="hasan@gmail.com", Phone="31212121212112" },
+            new() { Id = id++, Name = "dgdgd", Email="hasan@gmail.com", Phone="31212121212112" },
+        };
+    }
+}
+
+
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestProject.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    public List<Student> Students { get; set; } = new List<Student>();
+    public void OnGet()
+    {
+        Students = Student.GetAllStudent();
+    }
+}
